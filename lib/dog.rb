542e2bd8aa925881612689107f070fa8f7e6863c
Canyon Mathis <canyon.mathis@gmail.com>
2020-02-04 01:32:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,18 @@
 # Your code goes here!
 class Dog
- 
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
- 
-  def name
-    @this_dogs_name
-  end
-end
-def bark
-  puts "woof"
-end
- 
-lassie = Dog.new
-lassie.name = "Lassie"
+    @name = dogs_name
+  end 
 
-puts lassie.name
+  def name
+    @name
+  end 
+  
+end 
+
+class Dog
+  def bark
+    puts "woof!"
+  end 
+  
+end 
